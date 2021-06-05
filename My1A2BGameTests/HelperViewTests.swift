bdd88c5a9f5895d2db0fb21b24bc54949f1ab086
MING-TA YANG <startover205@gmail.com>
2021-06-05 16:45:58 +0800

    add test file for `HelperView`

---
+++ My1A2BGameTests/HelperViewTests.swift
@@ -0,0 +1,12 @@
+//
+//  HelperViewTests.swift
+//  My1A2BGameTests
+//
+//  Created by Ming-Ta Yang on 2021/6/5.
+//  Copyright © 2021 Ming-Ta Yang. All rights reserved.
+//
+
+import XCTest
+
+class HelperViewTests: XCTestCase {
+}
