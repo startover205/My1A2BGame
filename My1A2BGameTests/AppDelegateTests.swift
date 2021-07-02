c7f77d91c5b8f37c716a4528841c6e726bf63b82
MING-TA YANG <startover205@gmail.com>
2021-07-02 15:03:40 +0800

    Add UI tests for tab setup

---
+++ My1A2BGameTests/AppDelegateTests.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegateTests.swift
+//  My1A2BGameTests
+//
+//  Created by Ming-Ta Yang on 2021/6/30.
+//  Copyright © 2021 Ming-Ta Yang. All rights reserved.
+//
+
+import XCTest
+@testable import My1A2BGame
+
+class AppDelegateTests: XCTestCase {
+}
