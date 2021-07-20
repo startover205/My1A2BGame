3b011c306f575aabbfbe60a11d660b36145d5478
MING-TA YANG <startover205@gmail.com>
2021-07-20 14:32:32 +0800

    MastermindEvaluator.evaluate returns zero misplaced count on correct guess

--- My1A2BGameTests/MastermindEvaluatorTests.swift
+++ My1A2BGameTests/MastermindEvaluatorTests.swift
@@ -13,26 +13,39 @@ public final class MastermindEvaluator {
         case lengthMismatch
     }
 
-    static func evaluate(_ guess: [Int], with answer: [Int]) throws {
+    static func evaluate(_ guess: [Int], with answer: [Int]) throws -> (correctCount: Int, misplacedCount: Int) {
         guard guess.count == answer.count else { throw Error.lengthMismatch }
 
+        return (0, 0)
     }
 }
 
 
 class MastermindEvaluatorTests: XCTestCase {
-    func test_evalute_throwsLengthMismatchErrorOnMismatchLengthInputs() {
+    func test_evaluate_throwsLengthMismatchErrorOnMismatchLengthInputs() {
         let guess = [1, 2, 3, 4]
         let answer = [1, 2, 3, 4, 5]
 
         var capturedError: Error?
         do {
-            try MastermindEvaluator.evaluate(guess, with: answer)
+            _ = try MastermindEvaluator.evaluate(guess, with: answer)
         } catch {
             capturedError = error
         }
 
         XCTAssertEqual(capturedError as NSError?, lengthMismatch())
+    }
+    
+    func test_evaluate_returnsZeroMisplacedCountOnCorrectGuess() throws {
+        let guesses = [[1, 2, 3, 4], [3, 5, 6, 7], [8, 5, 3, 9], [0, 0, 0, 0], [9, 9, 9, 9]]
+        let answers = [[1, 2, 3, 4], [3, 5, 6, 7], [8, 5, 3, 9], [0, 0, 0, 0], [9, 9, 9, 9]]
+        
+        try guesses.enumerated().forEach { index, guess in
+            let answer = answers[index]
+            
+            let misplacedCount = try MastermindEvaluator.evaluate(guess, with: answer).misplacedCount
+            XCTAssertEqual(misplacedCount, 0)
+        }
     }
     
     // MARK: - Helpers
