e8617026a163f12520dd0f7f26c0de41142b881c
MING-TA YANG <startover205@gmail.com>
2021-07-20 15:11:17 +0800

    MastermindEvaluator.evaluate throws error on duplicate number inputs

--- My1A2BGameTests/MastermindEvaluatorTests.swift
+++ My1A2BGameTests/MastermindEvaluatorTests.swift
@@ -11,10 +11,14 @@ import XCTest
 public final class MastermindEvaluator {
     public enum Error: Swift.Error {
         case lengthMismatch
+        case duplicateNumber
     }
 
     static func evaluate(_ guess: [Int], with answer: [Int]) throws -> (correctCount: Int, misplacedCount: Int) {
         guard guess.count == answer.count else { throw Error.lengthMismatch }
+        guard guess.count == Set(guess).count else { throw Error.duplicateNumber }
+        guard answer.count == Set(answer).count else { throw Error.duplicateNumber }
+
 
         return (0, 0)
     }
@@ -31,9 +35,27 @@ class MastermindEvaluatorTests: XCTestCase {
         )
     }
     
+    func test_evaluate_throwsErrorOnDuplicateNumberAnswerInput() {
+        let guess = [1, 2, 3, 4]
+        let answer = [1, 1, 3, 4]
+
+        XCTAssertThrowsError(
+            try MastermindEvaluator.evaluate(guess, with: answer)
+        )
+    }
+    
+    func test_evaluate_throwsErrorOnDuplicateNumberGuessInput() {
+        let guess = [1, 1, 3, 4]
+        let answer = [1, 2, 3, 4]
+
+        XCTAssertThrowsError(
+            try MastermindEvaluator.evaluate(guess, with: answer)
+        )
+    }
+    
     func test_evaluate_returnsZeroMisplacedCountOnCorrectGuess() throws {
-        let guesses = [[1, 2, 3, 4], [3, 5, 6, 7], [8, 5, 3, 9], [0, 0, 0, 0], [9, 9, 9, 9]]
-        let answers = [[1, 2, 3, 4], [3, 5, 6, 7], [8, 5, 3, 9], [0, 0, 0, 0], [9, 9, 9, 9]]
+        let guesses = [[1, 2, 3, 4], [3, 5, 6, 7], [8, 5, 3, 9]]
+        let answers = [[1, 2, 3, 4], [3, 5, 6, 7], [8, 5, 3, 9]]
         
         try guesses.enumerated().forEach { index, guess in
             let answer = answers[index]
