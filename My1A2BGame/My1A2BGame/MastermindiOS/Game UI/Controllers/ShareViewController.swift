3b3dd867578e245e9e65ec52b46cf6b0d0749e36
MING-TA YANG <startover205@gmail.com>
2021-08-09 10:33:20 +0800

    Move `ShareViewController` to a standalone file

---
+++ My1A2BGame/My1A2BGame/MastermindiOS/Game UI/Controllers/ShareViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ShareViewController.swift
+//  My1A2BGame
+//
+//  Created by Ming-Ta Yang on 2021/8/9.
+//  Copyright © 2021 Ming-Ta Yang. All rights reserved.
+//
+
+import UIKit
+
+public final class ShareViewController {
+    private(set) lazy var view: UIBarButtonItem = {
+        let view = UIBarButtonItem(barButtonSystemItem: .organize, target: self, action: #selector(share))
+        return view
+    }()
+    
+    private weak var hostViewController: UIViewController?
+    private let guessCount: () -> Int
+    
+    public init(hostViewController: UIViewController, guessCount: @escaping () -> Int) {
+        self.hostViewController = hostViewController
+        self.guessCount = guessCount
+    }
+    
+    @objc func share() {
+        guard let hostViewController = hostViewController else { return }
+        let guessCount = guessCount()
+        
+        let format = NSLocalizedString("I won 1A2B Fun! with guessing only %d times! Come challenge me!", comment: "8th")
+        var activityItems: [Any] = [String.localizedStringWithFormat(format, guessCount)]
+        activityItems.append(Constants.appStoreDownloadUrl)
+        
+        if let snapshotView = hostViewController.view {
+            UIGraphicsBeginImageContextWithOptions(snapshotView.bounds.size, false, UIScreen.main.scale)
+            snapshotView.drawHierarchy(in: snapshotView.bounds, afterScreenUpdates: true)
+            if let screenShotImage = UIGraphicsGetImageFromCurrentImageContext() {
+                activityItems.append(screenShotImage)
+            }
+            UIGraphicsEndImageContext()
+        }
+        
+        let controller = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
+        controller.popoverPresentationController?.barButtonItem = view
+        hostViewController.present(controller, animated: true)
+    }
+}
--- My1A2BGame/My1A2BGame/MastermindiOS/Game UI/Controllers/WinViewController.swift
+++ My1A2BGame/My1A2BGame/MastermindiOS/Game UI/Controllers/WinViewController.swift
@@ -8,44 +8,6 @@
 
 import UIKit
 
-public final class ShareViewController {
-    private(set) lazy var view: UIBarButtonItem = {
-        let view = UIBarButtonItem(barButtonSystemItem: .organize, target: self, action: #selector(share))
-        return view
-    }()
-    
-    private weak var hostViewController: UIViewController?
-    private let guessCount: () -> Int
-    
-    public init(hostViewController: UIViewController, guessCount: @escaping () -> Int) {
-        self.hostViewController = hostViewController
-        self.guessCount = guessCount
-    }
-    
-    @objc func share() {
-        guard let hostViewController = hostViewController else { return }
-        let guessCount = guessCount()
-        
-        let format = NSLocalizedString("I won 1A2B Fun! with guessing only %d times! Come challenge me!", comment: "8th")
-        var activityItems: [Any] = [String.localizedStringWithFormat(format, guessCount)]
-        activityItems.append(Constants.appStoreDownloadUrl)
-        
-        if let snapshotView = hostViewController.view {
-            UIGraphicsBeginImageContextWithOptions(snapshotView.bounds.size, false, UIScreen.main.scale)
-            snapshotView.drawHierarchy(in: snapshotView.bounds, afterScreenUpdates: true)
-            if let screenShotImage = UIGraphicsGetImageFromCurrentImageContext() {
-                activityItems.append(screenShotImage)
-            }
-            UIGraphicsEndImageContext()
-        }
-        
-        let controller = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
-        controller.popoverPresentationController?.barButtonItem = view
-        hostViewController.present(controller, animated: true)
-    }
-}
-
-
 public protocol WinnerStore {
     var totalCount: Int { get }
     
