ed78378756762a16c567bb40b7a5c470b236367b
MING-TA YANG <startover205@gmail.com>
2021-08-13 13:51:38 +0800

    Move `anyNSError` helper method to a shared scope

---
+++ Mastermind/MastermindTests/Helpers/SharedTestHelpers.swift
@@ -0,0 +1,11 @@
+//
+//  SharedTestHelpers.swift
+//  MastermindTests
+//
+//  Created by Ming-Ta Yang on 2021/8/13.
+//
+
+import Foundation
+
+func anyNSError() -> NSError { NSError(domain: "any error", code: 0) }
+
