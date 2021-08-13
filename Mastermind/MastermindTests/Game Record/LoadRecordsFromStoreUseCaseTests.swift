f139da9fa9218e31cb4c4aa3b9fc8eb1fe23bac0
MING-TA YANG <startover205@gmail.com>
2021-08-13 13:57:57 +0800

    Add test case for validate new record

--- Mastermind/MastermindTests/Game Record/LoadRecordsFromStoreUseCaseTests.swift
+++ Mastermind/MastermindTests/Game Record/LoadRecordsFromStoreUseCaseTests.swift
@@ -8,7 +8,6 @@
 import XCTest
 import Mastermind
 
-
 class LoadRecordsFromStoreUseCaseTests: XCTestCase {
     func test_init_doesNotMessageStoreUponCreation() {
         let (_, store) = makeSUT()
@@ -98,53 +97,6 @@ class LoadRecordsFromStoreUseCaseTests: XCTestCase {
         trackForMemoryLeaks(sut, file: file, line: line)
         
         return (sut, store)
-    }
-    
-    private func anyNSError() -> NSError { NSError(domain: "any error", code: 0) }
-    
-    private final class RecordStoreSpy: RecordStore {
-        enum Message: Equatable {
-            case loadCount
-            case loadRecords
-        }
-        
-        private(set) var receivedMessages = [Message]()
-        private var retrievalCountResult: Result<Int, Error>?
-        private var retrievalRecordsResult: Result<[PlayerRecord], Error>?
-        
-        func totalCount() throws -> Int {
-            receivedMessages.append(.loadCount)
-            return try retrievalCountResult?.get() ?? 0
-        }
-        
-        func retrieve() throws -> [PlayerRecord] {
-            receivedMessages.append(.loadRecords)
-            return try retrievalRecordsResult?.get() ?? []
-        }
-        
-        func completeCountRetrieval(with error: Error) {
-            retrievalCountResult = .failure(error)
-        }
-        
-        func completeCountRetrieval(with count: Int) {
-            retrievalCountResult = .success(count)
-        }
-        
-        func completeCountRetrievalWithEmptyStore() {
-            retrievalCountResult = .success(0)
-        }
-        
-        func completeRecordsRetrieval(with error: Error) {
-            retrievalRecordsResult = .failure(error)
-        }
-        
-        func completeRecordsRetrieval(with records: [PlayerRecord]) {
-            retrievalRecordsResult = .success(records)
-        }
-        
-        func completeRecordsRetrievalWithEmptyStore() {
-            retrievalRecordsResult = .success([])
-        }
     }
 }
 
---
+++ Mastermind/MastermindTests/Game Record/ValidateNewRecordFromStoreUseCaseTests.swift
@@ -0,0 +1,30 @@
+//
+//  ValidateNewRecordFromStoreUseCaseTests.swift
+//  MastermindTests
+//
+//  Created by Ming-Ta Yang on 2021/8/13.
+//
+
+import XCTest
+import Mastermind
+
+class ValidateNewRecordFromStoreUseCaseTests: XCTestCase {
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+    
+    // MARK: Helpers
+    
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (RecordLoader, RecordStoreSpy) {
+        let store = RecordStoreSpy()
+        let sut = RecordLoader(store: store)
+        
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        
+        return (sut, store)
+    }
+    
+}
