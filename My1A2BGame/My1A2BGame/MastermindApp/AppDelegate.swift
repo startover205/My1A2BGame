a0767b6aa49389411785965f7757d410822f96b8
MING-TA YANG <startover205@gmail.com>
2021-08-20 10:32:57 +0800

    Move `WinUIComposer` to a standalone file

--- My1A2BGame/My1A2BGame/MastermindApp/AppDelegate.swift
+++ My1A2BGame/My1A2BGame/MastermindApp/AppDelegate.swift
@@ -12,7 +12,6 @@ import GoogleMobileAds
 import AppTrackingTransparency
 import AdSupport
 import Mastermind
-import GameKit
 
 
 @UIApplicationMain
@@ -132,108 +131,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let recordLoader = LocalRecordLoader(store: store)
         let winScene = WinUIComposer.winComposedWith(gameVersion: AdvancedGame(), userDefaults: .standard, recordLoader: recordLoader)
         basicGameNavigationController.pushViewController(winScene, animated: true)
-    }
-}
-
-public final class WinUIComposer {
-    private init() {}
-    
-    public static func winComposedWith(gameVersion: GameVersion, userDefaults: UserDefaults, recordLoader: RecordLoader) -> WinViewController {
-        
-        let winViewController = makeWinViewController()
-        let recordViewController = winViewController.recordViewController!
-        recordViewController.hostViewController = winViewController
-        recordViewController.loader = recordLoader
-        recordViewController.guessCount = { [unowned winViewController] in winViewController.guessCount }
-        recordViewController.spentTime = { [unowned winViewController] in winViewController.spentTime }
-        recordViewController.currentDate = Date.init
-        
-        winViewController.digitCount = gameVersion.digitCount
-        winViewController.userDefaults = userDefaults
-        winViewController.askForReview = { completion in
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-                SKStoreReviewController.requestReview()
-                completion()
-            }
-        }
-        winViewController.showFireworkAnimation = showFireworkAnimation(on:)
-        
-        let shareViewController = ShareViewController(
-            hostViewController: winViewController,
-            guessCount: { [unowned winViewController] in winViewController.guessCount })
-        winViewController.shareViewController = shareViewController
-        
-        return winViewController
-    }
-    
-    private static func makeWinViewController() -> WinViewController {
-        let winController = UIStoryboard(name: "Game", bundle: .init(for: WinViewController.self)).instantiateViewController(withIdentifier: "WinViewController") as! WinViewController
-        
-        return winController
-    }
-    
-    private static func showFireworkAnimation(on view: UIView) {
-        func showFirework(on view: UIView){
-            var cellsForFirework = [CAEmitterCell]()
-            
-            let cellRect = CAEmitterCell()
-            let cellHeart = CAEmitterCell()
-            let cellStar = CAEmitterCell()
-            
-            cellsForFirework.append(cellRect)
-            cellsForFirework.append(cellStar)
-            cellsForFirework.append(cellHeart)
-            
-            for cell in cellsForFirework {
-                cell.birthRate = 4500
-                cell.lifetime = 2
-                cell.velocity = 100
-                cell.scale = 0
-                cell.scaleSpeed = 0.2
-                cell.yAcceleration = 30
-                cell.color = #colorLiteral(red: 1, green: 0.8302680122, blue: 0.3005099826, alpha: 1)
-                cell.greenRange = 20
-                cell.spin = CGFloat.pi
-                cell.spinRange = CGFloat.pi * 3/4
-                cell.emissionRange = CGFloat.pi
-                cell.alphaSpeed = -1 / cell.lifetime
-                
-                cell.beginTime = CACurrentMediaTime()
-                cell.timeOffset = 1
-            }
-            
-            cellStar.contents = #imageLiteral(resourceName: "flake_star").cgImage
-            cellHeart.contents = #imageLiteral(resourceName: "flake_heart").cgImage
-            cellRect.contents = #imageLiteral(resourceName: "flake_rectangle").cgImage
-            
-            let emitterLayer = CAEmitterLayer()
-            
-            let randomDistribution = GKRandomDistribution(lowestValue: 1, highestValue: 8)
-            
-            var randomX = Double(randomDistribution.nextInt()) / 9
-            var randomY = Double(randomDistribution.nextInt()) / 9
-            
-            while randomX <= 7/9 , randomX >= 2/9, randomY <= 7/9, randomY >= 2/9 {
-                randomX = Double(randomDistribution.nextInt()) / 9
-                randomY = Double(randomDistribution.nextInt()) / 9
-            }
-            
-            emitterLayer.emitterPosition = CGPoint(x: view.frame.width * CGFloat(randomX) , y: view.frame.height * CGFloat(randomY))
-            
-            emitterLayer.emitterCells = cellsForFirework
-            emitterLayer.renderMode = CAEmitterLayerRenderMode.oldestLast
-            view.layer.insertSublayer(emitterLayer, at: 0)
-            
-            DispatchQueue.main.asyncAfter(deadline: .now() + 3) { [weak emitterLayer] in
-                emitterLayer?.removeFromSuperlayer()
-            }
-        }
-        
-        for i in 0...20 {
-            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i) * 0.5) { [weak view] in
-                guard let view = view else { return }
-                showFirework(on: view)
-            }
-        }
     }
 }
