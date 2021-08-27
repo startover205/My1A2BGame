331f0e5d726d97ffa085c0d55810bbf18fef4ac7
MING-TA YANG <startover205@gmail.com>
2021-08-27 16:03:30 +0800

    Create a public interface `Challenge` for client to handle game flow without knowing the existence of `Flow`

---
+++ Mastermind/Mastermind/Engine/Challenge.swift
@@ -0,0 +1,20 @@
+//
+//  Challenge.swift
+//  Mastermind
+//
+//  Created by Ming-Ta Yang on 2021/8/26.
+//
+
+public final class Challenge {
+    private init(flow: Any) {
+        self.flow = flow
+    }
+    
+    private let flow: Any
+    
+    public static func start<Secret, Delegate: FlowDelegate>(secret: Secret, maxChanceCount: Int, matchGuess: @escaping GuessMatcher<Delegate, Secret>, delegate: Delegate) -> Challenge  {
+        let flow = Flow(maxChanceCount: maxChanceCount, secret: secret, matchGuess: matchGuess, delegate: delegate)
+        flow.start()
+        return Challenge(flow: flow)
+    }
+}
---
+++ Mastermind/MastermindTests/Engine/ChallengeTests.swift
@@ -0,0 +1,51 @@
+//
+//  ChallengeTests.swift
+//  MastermindTests
+//
+//  Created by Ming-Ta Yang on 2021/8/25.
+//
+
+import XCTest
+import Mastermind
+
+class ChallengeTests: XCTestCase {
+    
+    private weak var sut: Challenge?
+    
+    func test_challenge_withTwoChance_requestDelegateToHandleWinWithProperHint() {
+        let (sut, delegate) = makeSUT(maxChanceCount: 2, matchGuess:  { _, _ in
+            return ("a hint about a success match", true)
+        })
+        self.sut = sut
+        
+        delegate.completions[0]("a correct guess")
+         
+        XCTAssertEqual(delegate.receivedMessages, [.acceptGuess(nil), .handleWin("a hint about a success match")])
+    }
+    
+    func test_challenge_withTwoChance_requestDelegateToHandleLoseWithProperHint() {
+        let (sut, delegate) = makeSUT(maxChanceCount: 1, matchGuess:  { _, _ in
+            return ("a hint about why it fails", false)
+        })
+        self.sut = sut
+        
+        delegate.completions[0]("an incorrect guess")
+        delegate.completions[0]("another incorrect guess")
+         
+        XCTAssertEqual(delegate.receivedMessages, [.acceptGuess(nil), .handleLose("a hint about why it fails"), .handleLose("a hint about why it fails")])
+    }
+    
+    // MARK: Helpers
+    
+    private func makeSUT(maxChanceCount: Int, matchGuess: @escaping GuessMatcher<DelegateSpy, String>, file: StaticString = #filePath, line: UInt = #line) -> (Challenge, DelegateSpy) {
+        let delegate = DelegateSpy()
+        let sut = Challenge.start(secret: "", maxChanceCount: maxChanceCount, matchGuess: matchGuess, delegate: delegate)
+
+        
+        trackForMemoryLeaks(delegate, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        
+        return (sut, delegate)
+    }
+
+}
