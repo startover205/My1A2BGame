38bc58c0f87f220544574ea30a0f43869df126fd
MING-TA YANG <startover205@gmail.com>
2021-08-30 10:21:24 +0800

    Create test helpers to improve readability

--- Mastermind/MastermindTests/Basic Challenge/DigitSecretMatcherTests.swift
+++ Mastermind/MastermindTests/Basic Challenge/DigitSecretMatcherTests.swift
@@ -29,22 +29,22 @@ final class DigitSecretMatcher {
 
 class DigitSecretMatcherTests: XCTestCase {
     func test_match_deliversFalseWithProperHintOnMatchingNoBullNoCowMatch() {
-        let secret = FourDigitSecret(digits: [1, 2, 3, 4])!
-        let guess = FourDigitSecret(digits: [5, 6, 7, 8])!
-        
-        let result = DigitSecretMatcher.match(guess, with: secret)
-        
-        XCTAssertEqual(result.hint, "0A0B")
-        XCTAssertFalse(result.correct)
+        expect(guess: [1, 2, 3, 4], matching: [5, 6, 7, 8], toCompleteWith: (hint: "0A0B", correct: false))
     }
     
     func test_match_deliversFalseWithProperHintOnMatchingNoBullOneCowMatch() {
-        let secret = FourDigitSecret(digits: [1, 2, 3, 4])!
-        let guess = FourDigitSecret(digits: [5, 1, 7, 8])!
+        expect(guess: [1, 2, 3, 4], matching: [5, 1, 7, 8], toCompleteWith: (hint: "0A1B", correct: false))
+    }
+    
+    // MARK: - Helpers
+
+    private func expect(guess: [Int], matching secret: [Int], toCompleteWith expectedResult: (hint: String, correct: Bool), file: StaticString = #filePath, line: UInt = #line) {
+        let secret = FourDigitSecret(digits: secret)!
+        let guess = FourDigitSecret(digits: guess)!
         
         let result = DigitSecretMatcher.match(guess, with: secret)
         
-        XCTAssertEqual(result.hint, "0A1B")
-        XCTAssertFalse(result.correct)
+        XCTAssertEqual(result.hint, expectedResult.hint, file: file, line: line)
+        XCTAssertEqual(result.correct, expectedResult.correct, file: file, line: line)
     }
 }
