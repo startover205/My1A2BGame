6fbb12dad06f008bf985875b5d47004dcc5facdb
MING-TA YANG <startover205@gmail.com>
2021-08-30 09:01:15 +0800

    Update tests to use test helper

--- Mastermind/MastermindTests/Basic Challenge/FourDigitSecret.swift
+++ Mastermind/MastermindTests/Basic Challenge/FourDigitSecret.swift
@@ -11,32 +11,39 @@ import Mastermind
 class FourDigitSecretTests: XCTestCase {
  
     func test_init_returnsNilOnRepeatedInputs() {
-        XCTAssertNil(FourDigitSecret(first: 0, second: 0, third: 0, fourth: 0))
-        XCTAssertNil(FourDigitSecret(first: 0, second: 0, third: 2, fourth: 3))
-        XCTAssertNil(FourDigitSecret(first: 0, second: 1, third: 1, fourth: 3))
-        XCTAssertNil(FourDigitSecret(first: 0, second: 1, third: 2, fourth: 2))
-        XCTAssertNil(FourDigitSecret(first: 3, second: 1, third: 2, fourth: 3))
+        XCTAssertNil(makeSUT(digits: [0, 0, 0, 0]))
+        XCTAssertNil(makeSUT(digits: [0, 1, 1, 3]))
+        XCTAssertNil(makeSUT(digits: [0, 1, 2, 2]))
+        XCTAssertNil(makeSUT(digits: [3, 1, 2, 3]))
     }
  
     func test_init_returnsNilOnNegativeInputs() {
-        XCTAssertNil(FourDigitSecret(first: -1, second: 1, third: 2, fourth: 3))
-        XCTAssertNil(FourDigitSecret(first: 0, second: -2, third: 2, fourth: 3))
-        XCTAssertNil(FourDigitSecret(first: 0, second: 1, third: -3, fourth: 3))
-        XCTAssertNil(FourDigitSecret(first: 0, second: 1, third: 2, fourth: -4))
+        XCTAssertNil(makeSUT(digits: [-1, 1, 2, 3]))
+        XCTAssertNil(makeSUT(digits: [0, -2, 2, 3]))
+        XCTAssertNil(makeSUT(digits: [0, 1, -3, 3]))
+        XCTAssertNil(makeSUT(digits: [0, 1, 2, -4]))
     }
     
     func test_init_returnsNilOnNonOneDigitInputs() {
-        XCTAssertNil(FourDigitSecret(first: 10, second: 1, third: 2, fourth: 3))
-        XCTAssertNil(FourDigitSecret(first: 0, second: 11, third: 2, fourth: 3))
-        XCTAssertNil(FourDigitSecret(first: 0, second: 1, third: 22, fourth: 3))
-        XCTAssertNil(FourDigitSecret(first: 0, second: 1, third: 2, fourth: 33))
+        XCTAssertNil(makeSUT(digits: [10, 1, 2, 3]))
+        XCTAssertNil(makeSUT(digits: [0, 11, 2, 3]))
+        XCTAssertNil(makeSUT(digits: [0, 1, 22, 3]))
+        XCTAssertNil(makeSUT(digits: [0, 1, 2, 33]))
     }
     
     func test_init_successfully() {
-        XCTAssertNotNil(FourDigitSecret(first: 0, second: 1, third: 2, fourth: 3))
-        XCTAssertNotNil(FourDigitSecret(first: 2, second: 3, third: 4, fourth: 5))
-        XCTAssertNotNil(FourDigitSecret(first: 9, second: 8, third: 7, fourth: 6))
-        XCTAssertNotNil(FourDigitSecret(first: 3, second: 9, third: 2, fourth: 0))
+        XCTAssertNotNil(makeSUT(digits: [0, 1, 2, 3]))
+        XCTAssertNotNil(makeSUT(digits: [2, 3, 4, 5]))
+        XCTAssertNotNil(makeSUT(digits: [9, 8, 7, 6]))
+        XCTAssertNotNil(makeSUT(digits: [3, 9, 2, 0]))
+    }
+    
+    // MARK: Helpers
+    
+    private func makeSUT(digits: [Int], file: StaticString = #filePath, line: UInt = #line) -> FourDigitSecret? {
+        let sut = FourDigitSecret(first: digits[0], second: digits[1], third: digits[2], fourth: digits[3])
+        
+        return sut
     }
 }
 
