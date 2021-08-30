009507d495c87fdbb1da9fea342773290be6f43f
MING-TA YANG <startover205@gmail.com>
2021-08-30 08:50:08 +0800

    `FourDigitSecret.init` can create object successfully

--- Mastermind/MastermindTests/BasicChallenge/FourDigitSecret.swift
+++ Mastermind/MastermindTests/BasicChallenge/FourDigitSecret.swift
@@ -8,10 +8,18 @@
 import XCTest
 
 struct FourDigitSecret {
-    let content: (Int, Int, Int, Int)
+    let content: [Int]
+    
+    static let digitCount = 4
     
     init?(first: Int, second: Int, third: Int, fourth: Int) {
-        return nil
+        content = [first, second, third, fourth]
+        
+        guard Set(content).count == Self.digitCount else { return nil }
+        
+        for digit in content {
+            if digit < 0 || digit > 9 { return nil }
+        }
     }
 }
 
@@ -38,6 +46,13 @@ class FourDigitSecretTests: XCTestCase {
         XCTAssertNil(FourDigitSecret(first: 0, second: 11, third: 2, fourth: 3))
         XCTAssertNil(FourDigitSecret(first: 0, second: 1, third: 22, fourth: 3))
         XCTAssertNil(FourDigitSecret(first: 0, second: 1, third: 2, fourth: 33))
+    }
+    
+    func test_init_successfully() {
+        XCTAssertNotNil(FourDigitSecret(first: 0, second: 1, third: 2, fourth: 3))
+        XCTAssertNotNil(FourDigitSecret(first: 2, second: 3, third: 4, fourth: 5))
+        XCTAssertNotNil(FourDigitSecret(first: 9, second: 8, third: 7, fourth: 6))
+        XCTAssertNotNil(FourDigitSecret(first: 3, second: 9, third: 2, fourth: 0))
     }
 }
 
