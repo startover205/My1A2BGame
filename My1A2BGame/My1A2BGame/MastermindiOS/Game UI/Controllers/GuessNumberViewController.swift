609c95773913a1d7bafa90d8018fb3ebf255b7fc
MING-TA YANG <startover205@gmail.com>
2021-08-31 14:38:18 +0800

    Move `HelperViewController` to its standalone file

--- My1A2BGame/My1A2BGame/MastermindiOS/Game UI/Controllers/GuessNumberViewController.swift
+++ My1A2BGame/My1A2BGame/MastermindiOS/Game UI/Controllers/GuessNumberViewController.swift
@@ -19,55 +19,6 @@ public protocol AdProvider {
     var rewardAd: GADRewardedAd? { get }
 }
 
-final class HelperViewController: NSObject {
-    @IBOutlet weak var helperBoardView: UIView!
-    @IBOutlet var helperNumberButtons: [HelperButton]!
-    
-    public var animate: Animate?
-    
-    func configureViews() {
-        helperNumberButtons.forEach {
-            $0.addTarget(self, action: #selector(helperNumberBtnPressed(_:)), for: .touchUpInside)
-        }
-    }
-    
-    @IBAction func helperBtnPressed(_ sender: Any) {
-        if helperBoardView.isHidden {
-            self.helperBoardView.isHidden = false
-            self.helperBoardView.transform = .init(translationX: 0, y: -300)
-            
-            animate?(0.25, {
-                self.helperBoardView.transform = .identity
-            }, nil)
-            
-        } else {
-            
-            animate?(0.25, {
-                self.helperBoardView.transform = .init(translationX: 0, y: -300)
-            }, { _ in
-                self.helperBoardView.isHidden = true
-            })
-        }
-    }
-    
-    @IBAction func helperInfoBtnPressed(_ sender: Any) {
-        AlertManager.shared.showConfirmAlert(.helperInfo)
-    }
-    
-    @IBAction func helperResetBtnPressed(_ sender: Any) {
-        helperNumberButtons.forEach { $0.reset() }
-    }
-    
-    @objc
-    func helperNumberBtnPressed(_ sender: HelperButton) {
-        sender.toggleColor()
-    }
-    
-    func hideView() {
-        helperBoardView.isHidden = true
-    }
-}
-
 public class GuessNumberViewController: UIViewController {
 
     public var gameVersion: GameVersion!
---
+++ My1A2BGame/My1A2BGame/MastermindiOS/Game UI/Controllers/HelperViewController.swift
@@ -0,0 +1,58 @@
+//
+//  HelperViewController.swift
+//  My1A2BGame
+//
+//  Created by Ming-Ta Yang on 2021/8/31.
+//  Copyright © 2021 Ming-Ta Yang. All rights reserved.
+//
+
+import UIKit
+
+final class HelperViewController: NSObject {
+    @IBOutlet weak var helperBoardView: UIView!
+    @IBOutlet var helperNumberButtons: [HelperButton]!
+    
+    public var animate: Animate?
+    
+    func configureViews() {
+        helperNumberButtons.forEach {
+            $0.addTarget(self, action: #selector(helperNumberBtnPressed(_:)), for: .touchUpInside)
+        }
+    }
+    
+    @IBAction func helperBtnPressed(_ sender: Any) {
+        if helperBoardView.isHidden {
+            self.helperBoardView.isHidden = false
+            self.helperBoardView.transform = .init(translationX: 0, y: -300)
+            
+            animate?(0.25, {
+                self.helperBoardView.transform = .identity
+            }, nil)
+            
+        } else {
+            
+            animate?(0.25, {
+                self.helperBoardView.transform = .init(translationX: 0, y: -300)
+            }, { _ in
+                self.helperBoardView.isHidden = true
+            })
+        }
+    }
+    
+    @IBAction func helperInfoBtnPressed(_ sender: Any) {
+        AlertManager.shared.showConfirmAlert(.helperInfo)
+    }
+    
+    @IBAction func helperResetBtnPressed(_ sender: Any) {
+        helperNumberButtons.forEach { $0.reset() }
+    }
+    
+    @objc
+    func helperNumberBtnPressed(_ sender: HelperButton) {
+        sender.toggleColor()
+    }
+    
+    func hideView() {
+        helperBoardView.isHidden = true
+    }
+}
