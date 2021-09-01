ec9928b1480432b3156359e6fbc067cd0a92bec3
MING-TA YANG <startover205@gmail.com>
2021-09-01 10:57:10 +0800

    Move `RewardAdViewController` to its own file

--- My1A2BGame/My1A2BGame/MastermindiOS/Game UI/Controllers/GuessNumberViewController.swift
+++ My1A2BGame/My1A2BGame/MastermindiOS/Game UI/Controllers/GuessNumberViewController.swift
@@ -18,55 +18,6 @@ public protocol AdProvider {
     var rewardAd: GADRewardedAd? { get }
 }
 
-public final class RewardAdViewController {
-    init(adProvider: AdProvider, adRewardChance: Int, countDownTime: TimeInterval, onGrantReward: @escaping () -> Void, hostViewController: UIViewController? = nil) {
-        self.adProvider = adProvider
-        self.adRewardChance = adRewardChance
-        self.countDownTime = countDownTime
-        self.onGrantReward = onGrantReward
-        self.hostViewController = hostViewController
-    }
-    
-    private let adProvider: AdProvider
-    private let adRewardChance: Int
-    private let countDownTime: TimeInterval
-    private let onGrantReward: () -> Void
-    
-    private weak var hostViewController: UIViewController?
-    
-    func adAvailable() -> Bool { adProvider.rewardAd != nil }
-    
-    func askUserToWatchAd(completion: @escaping (Bool) -> Void) {
-        let format = NSLocalizedString("Do you want to watch a reward ad? Watching a reward ad will grant you %d chances!", comment: "")
-        let message = String.localizedStringWithFormat(format, adRewardChance)
-        let alert = AlertAdCountdownController(
-            title: NSLocalizedString("You Are Out Of Chances...", comment: "2nd"),
-            message: message,
-            cancelMessage: NSLocalizedString("No, thank you", comment: "7th"),
-            countDownTime: countDownTime,
-            adHandler: { [weak self] in
-                completion(true)
-                self?.presentAd()
-            },
-            cancelHandler: {
-                completion(false)
-            })
-    
-        hostViewController?.present(alert, animated: true)
-    }
-    
-    private func presentAd() {
-        guard let ad = adProvider.rewardAd, let hostVC = hostViewController else { return }
-        
-        ad.present(fromRootViewController: hostVC) { [weak self] in
-            // We need to keep the reference of the ad so the callback can be fired correctly.
-            _ = ad
-            
-            self?.onGrantReward()
-        }
-    }
-}
-
 public class GuessNumberViewController: UIViewController {
 
     public var gameVersion: GameVersion!
