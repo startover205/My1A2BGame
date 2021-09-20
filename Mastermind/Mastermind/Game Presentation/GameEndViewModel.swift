1f0b79b47afadc63c14eb0365eaafe5d48545962
MING-TA YANG <startover205@gmail.com>
2021-09-20 21:48:44 +0800

    Move `GameEndViewModel` to its standalone file

---
+++ Mastermind/Mastermind/Game Presentation/GameEndViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  GameEndViewModel.swift
+//  Mastermind
+//
+//  Created by Ming-Ta Yang on 2021/9/20.
+//
+
+public struct GameEndViewModel {
+    public let voiceMessage: String
+}
--- Mastermind/Mastermind/Game Presentation/GameView.swift
+++ Mastermind/Mastermind/Game Presentation/GameView.swift
@@ -5,10 +5,6 @@
 //  Created by Ming-Ta Yang on 2021/9/19.
 //
 
-public struct GameEndViewModel {
-    public let voiceMessage: String
-}
-
 public protocol GameView {
     func display(_ viewModel: MatchResultViewModel)
     func display(_ viewModel: LeftChanceCountViewModel)
