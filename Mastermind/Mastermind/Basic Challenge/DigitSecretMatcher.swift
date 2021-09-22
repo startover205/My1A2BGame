9e62551a0e426e17d3300bbf8d08714971e7bd57
MING-TA YANG <startover205@gmail.com>
2021-09-22 15:12:24 +0800

    Remove unused `DigitSecretMatcher.match` method and its tests

--- Mastermind/Mastermind/Basic Challenge/DigitSecretMatcher.swift
+++ Mastermind/Mastermind/Basic Challenge/DigitSecretMatcher.swift
@@ -22,24 +22,6 @@ public struct MatchResult: Equatable {
 public final class DigitSecretMatcher {
     private init() {}
     
-    @available(*, deprecated)
-    public static func match(_ guess: DigitSecret, with secret: DigitSecret) -> (hint: String, correct: Bool) {
-        guard guess.content.count == secret.content.count else { return ("mismatch length", false) }
-        
-        var correctCount = 0
-        var misplacedCount = 0
-        
-        guess.content.enumerated().forEach { guessIndex, guessDigit in
-            secret.content.enumerated().forEach { answerIndex, answerDigit in
-                if guessDigit == answerDigit {
-                    guessIndex == answerIndex ? (correctCount += 1) : (misplacedCount += 1)
-                }
-            }
-        }
-        
-        return ("\(correctCount)A\(misplacedCount)B", correctCount == secret.content.count)
-    }
-    
     public static func matchGuess(_ guess: DigitSecret, with secret: DigitSecret) -> MatchResult {
         guard guess.content.count == secret.content.count else { return .init(bulls: 0, cows: 0, correct: false) }
         
