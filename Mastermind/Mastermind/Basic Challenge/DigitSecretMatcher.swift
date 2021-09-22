8391fa77900e8c2382bafea71d0ebe7a9140eaaf
MING-TA YANG <startover205@gmail.com>
2021-09-22 14:54:49 +0800

    Create new method `matchGuess` to return `MatchResult`

--- Mastermind/Mastermind/Basic Challenge/DigitSecretMatcher.swift
+++ Mastermind/Mastermind/Basic Challenge/DigitSecretMatcher.swift
@@ -7,6 +7,12 @@
 
 import Foundation
 
+public struct MatchResult {
+    let bulls: Int
+    let cows: Int
+    let correct: Bool
+}
+
 public final class DigitSecretMatcher {
     private init() {}
     
@@ -25,5 +31,22 @@ public final class DigitSecretMatcher {
         }
         
         return ("\(correctCount)A\(misplacedCount)B", correctCount == secret.content.count)
+    }
+    
+    public static func matchGuess(_ guess: DigitSecret, with secret: DigitSecret) -> MatchResult {
+        guard guess.content.count == secret.content.count else { return .init(bulls: 0, cows: 0, correct: false) }
+        
+        var correctCount = 0
+        var misplacedCount = 0
+        
+        guess.content.enumerated().forEach { guessIndex, guessDigit in
+            secret.content.enumerated().forEach { answerIndex, answerDigit in
+                if guessDigit == answerDigit {
+                    guessIndex == answerIndex ? (correctCount += 1) : (misplacedCount += 1)
+                }
+            }
+        }
+        
+        return .init(bulls: correctCount, cows: misplacedCount, correct: correctCount == secret.content.count)
     }
 }
