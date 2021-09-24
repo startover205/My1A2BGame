25ec098a4c78a5e1a5f9c326b189e72ae551f2d4
MING-TA YANG <startover205@gmail.com>
2021-09-24 09:37:53 +0800

    `GamePresenter.didUpdateLeftChanceCount` displays left chance count message

--- Mastermind/MastermindTests/Game Presentation/GamePresenterTests.swift
+++ Mastermind/MastermindTests/Game Presentation/GamePresenterTests.swift
@@ -7,9 +7,33 @@
 
 import XCTest
 
+public struct LeftChanceCountViewModel {
+    public let message: String
+    public let shouldBeAwareOfChanceCount: Bool
+}
+
+private protocol GameView {
+    func display(_ viewModel: LeftChanceCountViewModel)
+}
+
 private final class GamePresenter {
-    init(view: Any) {
-        
+    private let gameView: GameView
+    
+    init(gameView: GameView) {
+        self.gameView = gameView
+    }
+    
+    public static var guessChanceCountFormat: String {
+        NSLocalizedString("%d_GUESS_CHANCE_COUNT_FORMAT",
+            tableName: "Game",
+            bundle: Bundle(for: GamePresenter.self),
+            comment: "Format for the left chance count")
+    }
+    
+    func didUpdateLeftChanceCount(_ leftChanceCount: Int) {
+        let message = String.localizedStringWithFormat(Self.guessChanceCountFormat, leftChanceCount)
+        let shouldBeAwareOfChanceCount = leftChanceCount <= 3
+        gameView.display(LeftChanceCountViewModel(message: message, shouldBeAwareOfChanceCount: shouldBeAwareOfChanceCount))
     }
 }
 
@@ -21,11 +45,23 @@ class GamePresenterTests: XCTestCase {
         XCTAssertTrue(view.receivedMessages.isEmpty)
     }
     
+    func test_didUpdateLeftChanceCount_displaysLeftChanceCountMessage() {
+        let (sut, view) = makeSUT()
+        
+        sut.didUpdateLeftChanceCount(4)
+        XCTAssertEqual(view.receivedMessages, [.display(leftCountMessage: String.localizedStringWithFormat(GamePresenter.guessChanceCountFormat, 4), shouldBeAwareOfLeftCount: false)], "Expect displaying left chance count message but don't have to remind user to be aware of the left chance count")
+        
+        sut.didUpdateLeftChanceCount(3)
+        XCTAssertEqual(view.receivedMessages, [
+                        .display(leftCountMessage: String.localizedStringWithFormat(GamePresenter.guessChanceCountFormat, 4), shouldBeAwareOfLeftCount: false),
+                        .display(leftCountMessage: String.localizedStringWithFormat(GamePresenter.guessChanceCountFormat, 3), shouldBeAwareOfLeftCount: true)], "Expect displaying left chance count message and remind user to be aware of the left chance count")
+    }
+    
     // MARK: Helpers
     
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (GamePresenter, ViewSpy) {
         let view = ViewSpy()
-        let sut = GamePresenter(view: view)
+        let sut = GamePresenter(gameView: view)
         
         trackForMemoryLeaks(view, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
@@ -33,7 +69,15 @@ class GamePresenterTests: XCTestCase {
         return (sut, view)
     }
     
-    private final class ViewSpy {
-        private(set) var receivedMessages = [Any]()
+    private final class ViewSpy: GameView {
+        enum Message: Equatable {
+            case display(leftCountMessage: String, shouldBeAwareOfLeftCount: Bool)
+        }
+        
+        private(set) var receivedMessages = [Message]()
+        
+        func display(_ viewModel: LeftChanceCountViewModel) {
+            receivedMessages.append(.display(leftCountMessage: viewModel.message, shouldBeAwareOfLeftCount: viewModel.shouldBeAwareOfChanceCount))
+        }
     }
 }
