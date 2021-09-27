9ea582f6db346a07081056c30606d8e0c3354be1
MING-TA YANG <startover205@gmail.com>
2021-09-27 11:19:11 +0800

    `VoicePromptOnAlertPresenter` has localized text

---
+++ Mastermind/MastermindTests/Game Presentation/VoicePromptOnAlertPresenterTests.swift
@@ -0,0 +1,60 @@
+//
+//  VoicePromptOnAlertPresenterTests.swift
+//  MastermindTests
+//
+//  Created by Ming-Ta Yang on 2021/9/27.
+//
+
+import XCTest
+
+final class VoicePromptOnAlertPresenter {
+    private init() {}
+    
+    public static var alertTitle: String {
+        NSLocalizedString("VOICE_PROMPT_ON_ALERT_TITLE",
+                          tableName: "VoicePrompt",
+                          bundle: Bundle(for: VoicePromptOnAlertPresenter.self),
+                          comment: "Alert title for turning on voice prompt function")
+    }
+    
+    public static var alertMessage: String {
+        NSLocalizedString("VOICE_PROMPT_ON_ALERT_MESSAGE",
+                          tableName: "VoicePrompt",
+                          bundle: Bundle(for: VoicePromptOnAlertPresenter.self),
+                          comment: "Alert message for turning on voice prompt function")
+    }
+    
+    public static var alertConfirmTitle: String {
+        NSLocalizedString("VOICE_PROMPT_ON_ALERT_CONFIRM_TITLE",
+                          tableName: "VoicePrompt",
+                          bundle: Bundle(for: VoicePromptOnAlertPresenter.self),
+                          comment: "Alert confirm title for turning on voice prompt function")
+    }
+}
+
+class VoicePromptOnAlertPresenterTests: XCTestCase {
+    
+    func test_alertTitle_isLocalized() {
+        XCTAssertEqual(VoicePromptOnAlertPresenter.alertTitle, localized("VOICE_PROMPT_ON_ALERT_TITLE"))
+    }
+
+    func test_alertMessage_isLocalized() {
+        XCTAssertEqual(VoicePromptOnAlertPresenter.alertMessage, localized("VOICE_PROMPT_ON_ALERT_MESSAGE"))
+    }
+
+    func test_alertConfirmTitle_isLocalized() {
+        XCTAssertEqual(VoicePromptOnAlertPresenter.alertConfirmTitle, localized("VOICE_PROMPT_ON_ALERT_CONFIRM_TITLE"))
+    }
+    
+    // MARK: Helpers
+    
+    private func localized(_ key: String, file: StaticString = #filePath, line: UInt = #line) -> String {
+        let table = "VoicePrompt"
+        let bundle = Bundle(for: VoicePromptOnAlertPresenter.self)
+        let value = bundle.localizedString(forKey: key, value: nil, table: table)
+        if value == key {
+            XCTFail("Missing localized string for key: \(key) in table: \(table)", file: file, line: line)
+        }
+        return value
+    }
+}
