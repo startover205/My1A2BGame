aa5977f01a0f7c7cadea6ec9e19be1218569716a
MING-TA YANG <startover205@gmail.com>
2021-09-27 15:09:52 +0800

    `RewardAdViewController.replenishChance` delivers zero if host view controller is nil

--- My1A2BGame/My1A2BGameTests/RewardAdViewControllerTests.swift
+++ My1A2BGame/My1A2BGameTests/RewardAdViewControllerTests.swift
@@ -9,6 +9,15 @@
 import XCTest
 
 public final class RewardAdViewController {
+    private weak var hostViewController: UIViewController?
+    
+    init(hostViewController: UIViewController) {
+        self.hostViewController = hostViewController
+    }
+    
+    func replenishChance(completion: @escaping (Int) -> Void) {
+        completion(0)
+    }
 }
 
 class RewardAdViewControllerTests: XCTestCase {
@@ -18,11 +27,20 @@ class RewardAdViewControllerTests: XCTestCase {
         XCTAssertTrue(hostVC.capturedPresentations.isEmpty)
     }
     
+    func test_replenishChance_deliversZeroIfHostVCIsNil() {
+        let (sut, _) = makeSUT()
+        var capturedChanceCount: Int?
+        
+        sut.replenishChance { capturedChanceCount = $0 }
+        
+        XCTAssertEqual(capturedChanceCount, 0)
+    }
+    
     // MARK: Helpers
     
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (RewardAdViewController, UIViewControllerPresentationSpy) {
         let hostVC = UIViewControllerPresentationSpy()
-        let sut = RewardAdViewController()
+        let sut = RewardAdViewController(hostViewController: hostVC)
         
         trackForMemoryLeaks(hostVC, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
