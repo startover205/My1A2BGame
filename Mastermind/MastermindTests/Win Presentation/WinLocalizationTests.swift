4a7899dff0a684c952d9701b6736d6ee43c6b2c0
MING-TA YANG <startover205@gmail.com>
2021-10-01 09:54:09 +0800

    Add localization tests for `Win` table

---
+++ Mastermind/MastermindTests/Win Presentation/WinLocalizationTests.swift
@@ -0,0 +1,18 @@
+//
+//  WinLocalizationTests.swift
+//  MastermindTests
+//
+//  Created by Ming-Ta Yang on 2021/10/1.
+//
+
+import XCTest
+import Mastermind
+
+class WinLocalizationTests: XCTestCase {
+    func test_localizedStrings_haveKeysAndValuesForAllSupportedLocalizations() {
+        let table = "Win"
+        let bundle = Bundle(for: WinPresenter.self)
+        
+        assertLocalizedKeyAndValuesExist(in: bundle, table)
+    }
+}
