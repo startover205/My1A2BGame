43086f1a0fda3da73b0d6e622e9238fb06ca9235
MING-TA YANG <startover205@gmail.com>
2021-10-25 14:04:21 +0800

    Modify access control for `FAQTableViewController

--- My1A2BGame/My1A2BGame/MastermindiOS/More UI/Controllers/FAQTableViewController.swift
+++ My1A2BGame/My1A2BGame/MastermindiOS/More UI/Controllers/FAQTableViewController.swift
@@ -8,32 +8,32 @@
 
 import UIKit
 
-class FAQTableViewController: UITableViewController {
+public final class FAQTableViewController: UITableViewController {
     
-    var sectionOpenStatus: [Int: Bool] = [:]
-    var cachedScrollPosition : CGFloat?
-
-    override func viewDidLoad() {
+    private var sectionOpenStatus: [Int: Bool] = [:]
+    private var cachedScrollPosition : CGFloat?
+    
+    public override func viewDidLoad() {
         super.viewDidLoad()
         
         initSectionOpenStatus()
     }
 
-    override func scrollViewDidScroll(_ scrollView: UIScrollView) {
+    public override func scrollViewDidScroll(_ scrollView: UIScrollView) {
         // Force the tableView to stay at scroll position until animation completes
         if cachedScrollPosition != nil {
             tableView.setContentOffset(CGPoint(x: 0, y: cachedScrollPosition!), animated: false)
         }
     }
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if indexPath.row == 0{
+    public override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
             sectionOpenStatus[indexPath.section] = !sectionOpenStatus[indexPath.section]!
             
             let cell = tableView.cellForRow(at: indexPath)
             cell?.accessoryView?.transform = sectionOpenStatus[indexPath.section]! ? .init(rotationAngle: CGFloat(-Float.pi / 2)) :.identity
             
-            self.cachedScrollPosition = self.tableView.contentOffset.y
+            cachedScrollPosition = tableView.contentOffset.y
             
             if #available(iOS 11.0, *) {
                 tableView.performBatchUpdates(nil, completion: nil)
@@ -42,12 +42,11 @@ class FAQTableViewController: UITableViewController {
                 tableView.endUpdates()
             }
             
-            self.cachedScrollPosition = nil
+            cachedScrollPosition = nil
         }
     }
     
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-
+    public override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         let defaultHeight = super.tableView(tableView, heightForRowAt: indexPath)
 
         guard indexPath.row != 0 else {
@@ -57,7 +56,7 @@ class FAQTableViewController: UITableViewController {
         return sectionOpenStatus[indexPath.section]! ? defaultHeight : 0
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = super.tableView(tableView, cellForRowAt: indexPath)
         
         cell.accessoryView = indexPath.row == 0 ? UIImageView(image: #imageLiteral(resourceName: "baseline_keyboard_arrow_left_black_18pt")) : nil
