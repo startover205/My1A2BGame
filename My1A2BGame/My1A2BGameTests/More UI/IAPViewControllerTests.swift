26a1a875354f4d0690437260346605274c600956
MING-TA YANG <startover205@gmail.com>
2021-10-28 18:25:15 +0800

    Add test file for `IAPViewControllerTests`

---
+++ My1A2BGame/My1A2BGameTests/More UI/IAPViewControllerTests.swift
@@ -0,0 +1,14 @@
+//
+//  IAPViewControllerTests.swift
+//  My1A2BGameTests
+//
+//  Created by Ming-Ta Yang on 2021/10/27.
+//  Copyright © 2021 Ming-Ta Yang. All rights reserved.
+//
+
+import XCTest
+@testable import My1A2BGame
+
+class IAPViewControllerTests: XCTestCase {
+
+}
