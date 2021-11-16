332f5026b13f6868eb4c32f887e5478740d7750b
MING-TA YANG <startover205@gmail.com>
2021-11-16 17:34:19 +0800

    Move `IAPCellController` to its own file

---
+++ My1A2BGame/My1A2BGame/MastermindiOS/More UI/Controllers/IAPCellController.swift
@@ -0,0 +1,38 @@
+//
+//  IAPCellController.swift
+//  My1A2BGame
+//
+//  Created by Ming-Ta Yang on 2021/11/16.
+//  Copyright © 2021 Ming-Ta Yang. All rights reserved.
+//
+
+import UIKit
+
+final class IAPCellController: NSObject {
+    private let product: Product
+    private let selection: () -> Void
+    
+    internal init(product: Product, selection: @escaping () -> Void) {
+        self.product = product
+        self.selection = selection
+    }
+}
+
+extension IAPCellController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "IAPProductCell", for: indexPath) as! IAPTableViewCell
+        
+        cell.productNameLabel.text = product.name
+        cell.productPriceLabel.text = product.price
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selection()
+    }
+}
--- My1A2BGame/My1A2BGame/MastermindiOS/More UI/Controllers/IAPViewController.swift
+++ My1A2BGame/My1A2BGame/MastermindiOS/More UI/Controllers/IAPViewController.swift
@@ -9,35 +9,6 @@
 import UIKit
 import StoreKit
 
-final class IAPCellController: NSObject {
-    private let product: Product
-    private let selection: () -> Void
-    
-    internal init(product: Product, selection: @escaping () -> Void) {
-        self.product = product
-        self.selection = selection
-    }
-}
-
-extension IAPCellController: UITableViewDataSource, UITableViewDelegate {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        1
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "IAPProductCell", for: indexPath) as! IAPTableViewCell
-        
-        cell.productNameLabel.text = product.name
-        cell.productPriceLabel.text = product.price
-        
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        selection()
-    }
-}
-
 public class IAPViewController: UITableViewController {
     
     @IBOutlet private(set) public weak var restorePurchaseButton: UIBarButtonItem!
