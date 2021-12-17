9a3e28bcce756cc325dfdead50e864e6db8edbeb
MING-TA YANG <startover205@gmail.com>
2021-12-17 14:37:36 +0800

    Move `ProductLoader` to production

---
+++ My1A2BGame/My1A2BGame/MastermindiOS/More UI/Controllers/ProductLoader.swift
@@ -0,0 +1,40 @@
+//
+//  ProductLoader.swift
+//  My1A2BGame
+//
+//  Created by Ming-Ta Yang on 2021/12/17.
+//  Copyright © 2021 Ming-Ta Yang. All rights reserved.
+//
+
+import StoreKit
+
+public class ProductLoader: NSObject {
+    private let makeRequest: (Set<String>) -> SKProductsRequest
+    private let getProductIDs: () -> Set<String>
+    private var loadingRequest: (request: SKProductsRequest, completion: ([SKProduct]) -> Void)?
+    
+    public init(makeRequest: @escaping (Set<String>) -> SKProductsRequest, productIDs: @escaping () -> Set<String>) {
+        self.makeRequest = makeRequest
+        self.getProductIDs = productIDs
+    }
+    
+    public func load(completion: @escaping ([SKProduct]) -> Void) {
+        let productIDs = getProductIDs()
+        guard !productIDs.isEmpty else {
+            completion([])
+            return
+        }
+        
+        let request = makeRequest(productIDs)
+        request.delegate = self
+        loadingRequest = (request, completion)
+        
+        request.start()
+    }
+}
+
+extension ProductLoader: SKProductsRequestDelegate {
+    public func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
+        loadingRequest?.completion(response.products)
+    }
+}
--- My1A2BGame/My1A2BGameTests/More UI/IAP/ProductLoaderTests.swift
+++ My1A2BGame/My1A2BGameTests/More UI/IAP/ProductLoaderTests.swift
@@ -8,38 +8,7 @@
 
 import XCTest
 import StoreKit
-
-class ProductLoader: NSObject {
-    private let makeRequest: (Set<String>) -> SKProductsRequest
-    private let getProductIDs: () -> Set<String>
-    private var loadingRequest: (request: SKProductsRequest, completion: ([SKProduct]) -> Void)?
-    
-    init(makeRequest: @escaping (Set<String>) -> SKProductsRequest, productIDs: @escaping () -> Set<String>) {
-        self.makeRequest = makeRequest
-        self.getProductIDs = productIDs
-    }
-    
-    func load(completion: @escaping ([SKProduct]) -> Void) {
-        let productIDs = getProductIDs()
-        guard !productIDs.isEmpty else {
-            completion([])
-            return
-        }
-        
-        let request = makeRequest(productIDs)
-        request.delegate = self
-        loadingRequest = (request, completion)
-        
-        request.start()
-    }
-}
-
-extension ProductLoader: SKProductsRequestDelegate {
-    public func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
-        loadingRequest?.completion(response.products)
-    }
-}
-
+import My1A2BGame
 
 class ProductLoaderTests: XCTestCase {
     
