6bf52a9b49818cefb1520204d3b54457319ebfaf
MING-TA YANG <startover205@gmail.com>
2021-12-25 14:59:42 +0800

    Check `isMainQueue` instead of checking `isMainThread since other queues may also use the main thread

--- My1A2BGame/My1A2BGame/MainQueueDispatchProductLoader.swift
+++ My1A2BGame/My1A2BGame/MainQueueDispatchProductLoader.swift
@@ -9,9 +9,22 @@
 import StoreKit
 
 public final class MainQueueDispatchProductLoader: IAPProductLoader {
+    private static let key = DispatchSpecificKey<UInt8>()
+    private static let value = UInt8.max
+    
+    public override init(makeRequest: @escaping (Set<String>) -> SKProductsRequest, getProductIDs: @escaping () -> Set<String>) {
+        super.init(makeRequest: makeRequest, getProductIDs: getProductIDs)
+        
+        DispatchQueue.main.setSpecific(key: Self.key, value: Self.value)
+    }
+    
+    private func isMainQueue() -> Bool {
+        DispatchQueue.getSpecific(key: Self.key) == Self.value
+    }
+    
     public override func load(completion: @escaping ([SKProduct]) -> Void) {
         super.load { result in
-            if Thread.isMainThread {
+            if self.isMainQueue() {
                 completion(result)
             } else {
                 DispatchQueue.main.async {
