cd8fbf8a69464c0940fbf794aa33f023b7dc9681
MING-TA YANG <startover205@gmail.com>
2022-01-13 17:08:36 +0800

    Add `GoogleAPIKeys` to git using the debug keys

---
+++ My1A2BGame/My1A2BGame/GoogleAds/GoogleAPIKeys.swift
@@ -0,0 +1,13 @@
+//
+//  GoogleAPIKeys.swift
+//  My1A2BGame
+//
+//  Created by Ming-Ta Yang on 2022/1/11.
+//  Copyright © 2022 Ming-Ta Yang. All rights reserved.
+//
+
+enum GoogleAPIKeys {
+    static let bottomAdID = "ca-app-pub-3940256099942544/2934735716"
+    static let rewardAdID = "ca-app-pub-1287774922601866/3704195420"
+}
+
