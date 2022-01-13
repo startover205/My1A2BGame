0484cda4186d50a98b4e8a1ce7fbb736038c9e33
MING-TA YANG <startover205@gmail.com>
2022-01-13 21:54:24 +0800

    Prolong wait time for product loading to prevent flaky test

--- My1A2BGame/My1A2BGameTests/IAPAcceptanceTests.swift
+++ My1A2BGame/My1A2BGameTests/IAPAcceptanceTests.swift
@@ -194,7 +194,7 @@ class IAPAcceptanceTests: XCTestCase {
     private func waitForProductLoading() {
         let exp = expectation(description: "wait for product loading")
         exp.isInverted = true
-        wait(for: [exp], timeout: 0.5)
+        wait(for: [exp], timeout: 1)
     }
     
     private func allProducts() -> [SKProduct] {
