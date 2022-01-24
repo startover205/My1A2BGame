16b76466ca3309a1c370af94bf3322784d08afcc
MING-TA YANG <startover205@gmail.com>
2022-01-24 16:21:27 +0800

    Add localization tests for `InfoPlist.strings`

--- My1A2BGame/My1A2BGameTests/LocalizationTests.swift
+++ My1A2BGame/My1A2BGameTests/LocalizationTests.swift
@@ -9,7 +9,27 @@
 import XCTest
 
 class LocalizationTests: XCTestCase {
-    func test_localizedStrings_haveKeysAndValuesForAllSupportedLocalizations() {
+    func test_infoPlist_localizedStrings_haveKeysAndValuesForAllSupportedLocalizations() {
+        let table = "InfoPlist"
+        let presentationBundle = Bundle.main
+        let localizationBundles = allLocalizationBundles(in: presentationBundle)
+        let localizedStringKeys = allLocalizedStringKeys(in: localizationBundles, table: table)
+        
+        localizationBundles.forEach { (bundle, localization) in
+            localizedStringKeys.forEach { key in
+
+                let localizedString = bundle.localizedString(forKey: key, value: nil, table: table)
+                
+                if localizedString == key {
+                    let language = Locale.current.localizedString(forLanguageCode: localization) ?? ""
+
+                    XCTFail("Missing \(language) (\(localization)) localized string for key: '\(key)' in table: '\(table)'")
+                }
+            }
+        }
+    }
+    
+    func test_localizable_localizedStrings_haveKeysAndValuesForAllSupportedLocalizations() {
         let table = "Localizable"
         let presentationBundle = Bundle.main
         let localizationBundles = allLocalizationBundles(in: presentationBundle)
